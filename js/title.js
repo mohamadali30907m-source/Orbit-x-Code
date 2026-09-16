8c59e9442e55b8d3f607e9d1886d3d067466a2c1
sarahzoghly <sarahzoghly1223@gmail.com>
2026-09-16 22:17:43 +0300

    added fullscreen button

--- js/title.js
+++ js/title.js
@@ -75,3 +75,28 @@ function blendColors(c0, c1, p) {
     var f=parseInt(c0.slice(1),16),t=parseInt(c1.slice(1),16),R1=f>>16,G1=f>>8&0x00FF,B1=f&0x0000FF,R2=t>>16,G2=t>>8&0x00FF,B2=t&0x0000FF;
     return "#"+(0x1000000+(Math.round((R2-R1)*p)+R1)*0x10000+(Math.round((G2-G1)*p)+G1)*0x100+(Math.round((B2-B1)*p)+B1)).toString(16).slice(1);
 }
+
+const fullscreen = document.getElementById('fullscreen');
+let entered_fullscreen = false;
+
+fullscreen.addEventListener('click', () => {
+    if (entered_fullscreen){
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } 
+        else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+        entered_fullscreen = false;
+    }
+    else{
+        const el = document.documentElement;
+        if (el.requestFullscreen) {
+            el.requestFullscreen();
+        } 
+        else if (el.webkitRequestFullscreen) {
+            el.webkitRequestFullscreen();
+        }
+        entered_fullscreen = true;
+    }
+});
