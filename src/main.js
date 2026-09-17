641fe84b7970eee91a53933a788c658a40386e56
sarahzoghly <sarahzoghly1223@gmail.com>
2026-09-18 00:31:59 +0300

    added the level page. Added sun

---
+++ src/main.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("#starfield").forEach(section => section.style.display = "block");
+document.querySelectorAll("#title-screen").forEach(section => section.style.display = "none");
