db2228657b9d6a00cc4038f7d5b8fb4b0a8248e2
sarahzoghly <sarahzoghly1223@gmail.com>
2026-09-20 14:19:32 +0300

    updated levels screen to have unlocked Earth

--- src/modules/levels-screen/levels.js
+++ src/modules/levels-screen/levels.js
@@ -26,3 +26,44 @@ lockedPlanets.forEach(planet => {
     });
 
 });
+
+
+const unlockedPlanets = document.querySelectorAll(".unlocked-planet");
+
+unlockedPlanets.forEach(planet => {
+
+    planet.addEventListener("mouseenter", () => {
+        tooltip.textContent = "Select Level!";
+        tooltip.style.display = "block";
+    });
+
+    planet.addEventListener("mousemove", (event) => {
+        tooltip.style.left = `${event.clientX + 15}px`;
+        tooltip.style.top = `${event.clientY + 15}px`;
+    });
+
+    planet.addEventListener("mouseleave", () => {
+        tooltip.style.display = "none";
+    });
+
+});
+
+const sun = document.querySelectorAll("#sun");
+
+sun.forEach(planet => {
+
+    planet.addEventListener("mouseenter", () => {
+        tooltip.textContent = "This star is hot!";
+        tooltip.style.display = "block";
+    });
+
+    planet.addEventListener("mousemove", (event) => {
+        tooltip.style.left = `${event.clientX + 15}px`;
+        tooltip.style.top = `${event.clientY + 15}px`;
+    });
+
+    planet.addEventListener("mouseleave", () => {
+        tooltip.style.display = "none";
+    });
+
+});
